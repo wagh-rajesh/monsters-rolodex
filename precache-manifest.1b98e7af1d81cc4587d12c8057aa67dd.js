e63d3833534148ed46da2d40eee766841069c247
Rajesh Wagh <rwaghgit@gmail.com>
2020-04-22 12:23:02 +0530

    Updates

---
+++ precache-manifest.1b98e7af1d81cc4587d12c8057aa67dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119d1a0e2aa4ea7934c172b06c279a38",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "13ec38645eb21b8ecf93",
+    "url": "/monsters-rolodex/static/css/main.6d6fc1fd.chunk.css"
+  },
+  {
+    "revision": "abd7d71b0a1f3859dfb8",
+    "url": "/monsters-rolodex/static/js/2.0adb66df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.0adb66df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ec38645eb21b8ecf93",
+    "url": "/monsters-rolodex/static/js/main.83725d52.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
